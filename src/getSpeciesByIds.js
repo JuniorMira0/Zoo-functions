af4f783b538b3ab84b7d98eb662b7b7ebfcb0ab4
JuniorMira <j-unyor14@hotmail.com>
2022-01-27 11:59:36 -0300

    requisito 1 e 2

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,8 @@
+// const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
-  // seu código aqui
+function getSpeciesByIds(...ids) {
+  return ids.map((id) => data.species.find((especie) => especie.id === id));
 }
 
 module.exports = getSpeciesByIds;
